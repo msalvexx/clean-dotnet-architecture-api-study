eaaf8804b717872e35f33e9e26bd780c827f9ae8
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-09 22:30:35 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/Presentation/Controllers/SignUp/SignUpController.cs
+++ src/Presentation/Controllers/SignUp/SignUpController.cs
@@ -7,11 +7,26 @@ namespace Presentation.Controllers.SignUp
 {
     public class SignUpController
     {
-        public async Task<IHttpResponse<object>> HandleAsync(ISignupRequest request) =>
-            await Task.Run(() => new
+        public async Task<IHttpResponse<object>> HandleAsync(ISignupRequest request)
+        {
+            var requiredFields = new[] { "Name", "Email", "Password", "PasswordConfirmation" };
+            foreach (var field in requiredFields)
             {
-                Body = new MissingParameterException("Name"),
-                Status = 400
-            }.ActLike<IHttpResponse<object>>());
+                var hasField = request.GetType().GetProperty(field).GetValue(request) != null;
+                if (!hasField)
+                {
+                    return await Task.Run(() => new
+                    {
+                        Body = new MissingParameterException(field),
+                        Status = 400
+                    }.ActLike<IHttpResponse<object>>());
+                }
+            }
+            return new
+            {
+                Body = "Ok",
+                Status = 200
+            }.ActLike<IHttpResponse<object>>();
+        }
     }
 }
