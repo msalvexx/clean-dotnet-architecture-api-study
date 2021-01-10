f28f2269cbf74e4700b137f49f3ba61d758f85f3
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-09 22:34:35 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/Test/Presentation/Controllers/SignUpControllerTest.cs
+++ src/Test/Presentation/Controllers/SignUpControllerTest.cs
@@ -37,5 +37,20 @@ namespace Test.Presentation.Controllers
             response.Status.Should().Be(400);
             response.Body.Should().BeEquivalentTo(new MissingParameterException("Email"));
         }
+
+        [Fact]
+        public static async Task ShouldReturn400WhenNoPasswordProvided()
+        {
+            var sut = new SignUpController();
+            var request = new SignUpRequest
+            {
+                Name = "any_name",
+                Email = "any_email@mail.com",
+                PasswordConfirmation = "any_password"
+            };
+            var response = await sut.HandleAsync(request);
+            response.Status.Should().Be(400);
+            response.Body.Should().BeEquivalentTo(new MissingParameterException("Password"));
+        }
     }
 }
