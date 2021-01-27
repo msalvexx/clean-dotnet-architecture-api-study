720be6bf88598c5c1f8635606e5f108fef6add0a
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-27 08:27:10 -0300

    test: ensure CompositeValidation throw if any validator in array throws

--- src/Test/Utils/Validations/ValidationCompositeTest.cs
+++ src/Test/Utils/Validations/ValidationCompositeTest.cs
@@ -1,4 +1,7 @@
+using System;
+using FluentAssertions;
 using Moq;
+using Presentation.Exceptions;
 using Presentation.Protocols;
 using Utils.Validators;
 using Xunit;
@@ -25,6 +28,21 @@ namespace Test.Utils
             sut.Validate(data);
             compareFieldsMock.Verify(x => x.Validate(It.IsAny<object>()), Times.Once);
             emailValidationMock.Verify(x => x.Validate(It.IsAny<object>()), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldThrowIfAnyValidatorThrows()
+        {
+            var compareFieldsMock = MakeValidatorMock<CompareFieldsValidation>();
+            var emailValidationMock = MakeValidatorMock<EmailValidation>();
+            compareFieldsMock.Setup(x => x.Validate(It.IsAny<object>())).Throws(new InvalidParameterException("Field"));
+            var sut = MakeSut(new[] { compareFieldsMock.Object, emailValidationMock.Object });
+            var data = new
+            {
+                Field = "any_field"
+            };
+            Action act = () => sut.Validate(data);
+            act.Should().Throw<Exception>();
         }
     }
 }
