a8fbbfdbd8a1bf9c3b6b9d5c8bf2fdd8b1258d39
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-26 21:15:25 -0300

    test: ensure EmailValidation calls email validator with correct email

--- src/Test/Utils/Validations/EmailValidationTest.cs
+++ src/Test/Utils/Validations/EmailValidationTest.cs
@@ -1,7 +1,9 @@
 using System;
 using FluentAssertions;
 using FluentAssertions.Common;
+using Moq;
 using Presentation.Exceptions;
+using Utils.Protocols;
 using Utils.Validators;
 using Xunit;
 
@@ -9,19 +11,37 @@ namespace Test.Utils
 {
     public class EmailValidationTest
     {
-        public static EmailValidation MakeSut() => new("Email");
+        public static EmailValidation MakeSut(IEmailValidator emailValidator) => new("Email", emailValidator);
+        public static Mock<IEmailValidator> MakeEmailValidatorMock() => new();
 
         [Fact]
         public static void ShouldThrowInvalidParameterExceptionIfEmailIsInvalid()
         {
-            var sut = MakeSut();
+            var emailValidatorMock = MakeEmailValidatorMock();
+            emailValidatorMock.Setup(x => x.IsValid(It.IsAny<string>())).Returns(false);
+            var sut = MakeSut(emailValidatorMock.Object);
             var request = new
             {
-                Email = "any_email",
+                Email = "wrong_email@mail.com",
                 Password = "any_password"
             };
             Action act = () => sut.Validate(request);
             act.Should().Throw<InvalidParameterException>().IsSameOrEqualTo(new InvalidParameterException("Email"));
+        }
+
+        [Fact]
+        public static void ShouldCallIEmailValidatorWithCorrectEmail()
+        {
+            var emailValidatorMock = MakeEmailValidatorMock();
+            emailValidatorMock.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);
+            var sut = MakeSut(emailValidatorMock.Object);
+            var request = new
+            {
+                Email = "any_email@mail.com",
+                Password = "any_password"
+            };
+            sut.Validate(request);
+            emailValidatorMock.Verify(x => x.IsValid("any_email@mail.com"));
         }
     }
 }
