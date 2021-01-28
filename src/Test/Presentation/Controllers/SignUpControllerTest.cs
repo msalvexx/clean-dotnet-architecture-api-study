f46da7cf1855d5f97031a35998c0b679e96fd06f
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-28 13:34:22 -0300

    test: ensure SignUpController returns bad request if any validator fails

--- src/Test/Presentation/Controllers/SignUpControllerTest.cs
+++ src/Test/Presentation/Controllers/SignUpControllerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain.Models;
 using Domain.UseCases;
@@ -37,6 +38,36 @@ namespace Test.Presentation.Controllers
             var response = await sut.HandleAsync(request);
             validatorMock.Verify(x => x.Validate(It.IsAny<object>()), Times.Once);
         }
+
+        [Theory]
+        [MemberData(nameof(GetValidationExceptions))]
+        public static async Task ShouldReturn400IfValidatorThrows(Exception e)
+        {
+            var validatorMock = MakeValidatorMock();
+            var addAccountMock = MakeAddAccountMock();
+            validatorMock.Setup(x => x.Validate(It.IsAny<object>())).Throws(e);
+            var sut = MakeSut(validatorMock, addAccountMock);
+            var request = new HttpRequest
+            {
+                Body = new SignUpRequest
+                {
+                    Name = "any_name",
+                    Email = "any_email@mail.com",
+                    Password = "any_password"
+                }
+            };
+            var response = await sut.HandleAsync(request);
+            response.Status.Should().Be(400);
+            response.Body.Should().BeOfType(e.GetType());
+        }
+
+        public static IEnumerable<object[]> GetValidationExceptions =>
+            new List<object[]>
+            {
+                new object[] { new InvalidParameterException("Field") },
+                new object[] { new MissingParameterException("Field") }
+            };
+
 
         [Fact]
         public static async Task ShouldReturn500IfValidatorThrowsOtherException()
