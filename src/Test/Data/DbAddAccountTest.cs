ed26672dd8fac1c9ddcb7a3bd1b37c9e7b25c6af
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-28 14:54:01 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/Test/Data/DbAddAccountTest.cs
+++ src/Test/Data/DbAddAccountTest.cs
@@ -11,8 +11,9 @@ namespace Test.Data
 {
     public class DbAddAccountTest
     {
+        private static Mock<IAddAccountRepository> MakeAddAccountRepository() => new();
         private static Mock<IHasher> MakeHasher() => new();
-        private static DbAddAccount MakeSut(Mock<IHasher> hasherMock) => new(hasherMock.Object);
+        private static DbAddAccount MakeSut(Mock<IHasher> hasherMock, Mock<IAddAccountRepository> addAccountRepositoryMock) => new(hasherMock.Object, addAccountRepositoryMock.Object);
 
         private static IAddAccountModel MakeData() => new
         {
@@ -24,8 +25,9 @@ namespace Test.Data
         [Fact]
         public void ShouldCallHasherWithCorrectValue()
         {
+            var addAccountMock = MakeAddAccountRepository();
             var hasherMock = MakeHasher();
-            var sut = MakeSut(hasherMock);
+            var sut = MakeSut(hasherMock, addAccountMock);
             var data = MakeData();
             sut.Add(data);
             hasherMock.Verify(x => x.Generate(data.Password), Times.Once);
@@ -34,12 +36,31 @@ namespace Test.Data
         [Fact]
         public void ShouldDbAddAccountThrowIfHasherThrows()
         {
+            var addAccountMock = MakeAddAccountRepository();
             var hasherMock = MakeHasher();
             hasherMock.Setup(x => x.Generate(It.IsAny<string>())).Throws(new Exception());
-            var sut = MakeSut(hasherMock);
+            var sut = MakeSut(hasherMock, addAccountMock);
             var data = MakeData();
             Action act = () => sut.Add(data);
             act.Should().Throw<Exception>();
+        }
+
+        [Fact]
+        public void ShouldCallIAddAccountRepositoryWithCorrectValues()
+        {
+            var addAccountMock = MakeAddAccountRepository();
+            var hasherMock = MakeHasher();
+            hasherMock.Setup(x => x.Generate(It.IsAny<string>())).Returns("hashed_value");
+            var sut = MakeSut(hasherMock, addAccountMock);
+            var data = MakeData();
+            sut.Add(data);
+            var expectedData = new AddAccountModel
+            {
+                Name = "any_name",
+                Email = "any_email@mail.com",
+                Password = "hashed_value"
+            };
+            addAccountMock.Verify(x => x.Add(It.Is<IAddAccountModel>(x => x.Equals(expectedData))), Times.Once);
         }
     }
 }
