cd645fab40ebb81a11fefae997222e4fcd008da7
Matheus Alves Arruda <matheus.arruda@radixeng.com.br>
2021-01-30 22:40:22 -0300

    chore: enable debug with docker-compose

--- src/Infra/Db/MongoDb/Models/MongoDbSettings.cs
+++ src/Infra/Db/MongoDb/Models/MongoDbSettings.cs
@@ -9,6 +9,6 @@ namespace Infra.Db.MongoDb.Models
     public class MongoDbSettings : IMongoDbSettings
     {
         public string DatabaseName { get; set; } = "clean-dotnet-api";
-        public string ConnectionString { get; set; } = "mongodb://localhost:27017";
+        public string ConnectionString { get; set; } = "mongodb://mongo:27017/clean-node-api";
     }
 }
